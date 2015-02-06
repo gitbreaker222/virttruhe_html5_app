8a1f5986512bfa983612ce036c59009dc047cdc0
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2015-02-06 13:08:32 +0100

    removed old status vars in status control

--- game/script/status_control.js
+++ game/script/status_control.js
@@ -9,32 +9,32 @@
 function change_status(status){
 	switch(status) {
 		case "title":
-	    	game_status = "pause"; //TODO remove old varible
-	    	global_status.game_status = "pause";
+	    	global_status.game_status = "title";
 	    	$("#status").attr("class", "hide");
 	    	hide_all_pages();
 			page_title.setAttribute("class", "show");
 	        break;
+	        
 	    case "scan":
-	    	game_status = "scan"; //TODO remove old varible
 	    	global_status.game_status = "scan";
 	    	hide_all_pages();
 	       	page_scan.setAttribute("class", "show");
 			unselect_all();
 	        break;
+	        
 	    case "inventory":
-	    	game_status = "inventory"; //TODO remove old varible
 	    	global_status.game_status = "inventory";
 	    	$("#status").attr("class", "show");
 	    	hide_all_pages();
 	        page_inventory.setAttribute("class", "show");
 	        break;
+	        
 	    case "pause":
-	    	game_status = "pause"; //TODO remove old varible
 	    	global_status.game_status = "pause";
 	    	hide_all_pages();
 			page_pause.setAttribute("class", "show");
 	        break;
+	        
 	    default:
 	    	console.log("Something is wrong. Set default status");
 	    	global_status.game_status = "inventory";
