0769255919450f23fed12e23c9cf7527e8159af1
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2015-03-06 17:55:38 +0100

    added comments in data and var vor items in inv

--- game/data/data.js
+++ game/data/data.js
@@ -1,34 +1,34 @@
 var data = {
 	
 	
-	"status"			: null,
+	"status"			: null, //the app status, e.g. "scanning" or "inventory" or "presentation"
 	
-	"prev_status"		: null,
+	"prev_status"		: null, //the previous app status, when changing. Used to choose the next status, when going back from pause menu etc.
 	
-	"layer"				: "title",
+	"layer"				: "title", //the virtual layer, e.g. "Reality" or "Kokiri Forest"
 	
 };
 
 
 var user_stats = {
 	
-	"hearts"			: 2,
+	"hearts"			: 2, // the players/users health bar
 	
-	"keys"				: 0,
+	"keys"				: 0, // keys are used to unlock portals or chests that are locked
 	
-	"rupees"			: 0,
+	"rupees"			: 0, // amount of the currency "rupees". Can be exchanged for items in the shop
 	
 };
 
 
 var content = {
 	
-	"selected_item"		: null,
+	"selected_item"		: null, // the ID of the currently selected item in the inventory
 	
 };
 
 
-var dialog = [
+var dialog = [	// informations that should be shown to the user. but only information, no format here. that is in html-objecs.js
 	{//page 1
 		"content"			: "",
 		"buttons"			: 
@@ -42,7 +42,7 @@ var dialog = [
 ];
 
 
-var preferences = {
+var preferences = {		// these are set in the pause menu
 	"music"				: true,
 	"sfx"				: true,
 	"photo_scanner"		: false,
---
+++ game/data/items_in_inventory.js
@@ -0,0 +1,1 @@
+var items_in_inventory = {};
