2a484c44d7c2722107443e16b1ef83560478b791
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2015-03-22 13:58:56 +0100

    inventory: added f -add -select -remove

--- game/logic/inventory.js
+++ game/logic/inventory.js
@@ -1,13 +1,47 @@
 inventory = {
-	"reload" : function() {
-		console.log("reload inventory");
-	},
+	"items"		: [],
+	"selected"	: null,
+	
+	
+	"reload"	: function() {
+					console.log("reload inventory");
+				},
 	
 	
 	
-	"add" : function (item) {
-		console.log("adding to inventory" + item);
-	},
+	"add"		: function (item) {
+					this.items.push(item);
+					return(this.items);
+				},
+	
+	
+	"remove"	: function (item) {
+					
+					var items = this.items;
+					
+					for (i in items){
+						if (items[i] == item){
+							//removes 1 item beginning at position i
+							items.splice(i, 1);
+							
+							if (this.selected == item){
+								this.select(null);
+							}
+							
+							return(items);
+						}
+					}
+										
+					console.log(item + " not in inventory");
+					return(items);
+					
+				},
+	
+	
+	"select"	: function (item) {
+					this.selected = item;
+					return(this.selected);
+				}
 	
 	
 };
