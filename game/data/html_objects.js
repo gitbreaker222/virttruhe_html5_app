90e84320ff2176310b3f9825ac4f3da3f33d5d49
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2015-03-26 22:44:46 +0100

    renamend id for user stats bar

--- game/data/html_objects.js
+++ game/data/html_objects.js
@@ -1,39 +1,27 @@
-/*
- * mostly jQuery objects. They get manipulated by a -logic- component,
- * after another -logic- component wrote the content for it in -data-
- * and visualized by -view-
- */
 page = {
 	/*
 	 * HEAD
 	 */
-	"layer"			: $("#layer"),
-	"music"			: $("#music_player"),
-	"sfx"			: $("#sfx_player"),
+	layer			: $("#layer"),
+	music			: $("#music_player"),
+	sfx				: $("#sfx_player"),
 	
 	/*
 	 * USER_STATS
 	 */
-	"life"			: $("#life"),
-	"keys"			: $("#keys"),
-	"rupees"		: $("#rupees"),
+	user_stats		: $("#user_stats"),
+	life			: $("#life"),
+	keys			: $("#keys"),
+	rupees			: $("#rupees"),
 	
 	/*
 	 * CONTENT
 	 */
-	"title"			: $("#title"),
-	"inventory"		: $("#inventory"),
-	"inventory_list": $("#items"),
-	"item_title"	: $("#item_title"),
-	"scan"			: $("#scan"),
-	"pause"			: $("#pause"),
-	"dialog"		: $("#dialog"),
+	title			: $("#title"),
+	inventory		: $("#inventory"),
+	items			: $("#items"),
+	item_title		: $("#item_title"),
+	scan			: $("#scan"),
+	pause			: $("#pause"),
+	dialog			: $("#dialog"),
 };
-
-
-//save the actual jQuery opject, instead of the link TODO check
-function save_objects(){
-	for (obj in page){
-		page.obj = obj; 
-	}
-}
