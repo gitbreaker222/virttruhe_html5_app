2b546d135ed22423e9d41c2b8f7f890c8e5a6a3c
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2015-05-14 16:07:11 +0200

    wired buttons ui fn

--- game/control/buttons.js
+++ game/control/buttons.js
@@ -1,4 +1,9 @@
+/*
+ * GLOBAL BUTTONS
+ */
 function btn_scan(){
+	console.log("pressed scan button");
+	
 	//load current state
 	var state = system_status.state;
 	var prev_state1 = system_status.prev_state1;
@@ -10,5 +15,54 @@ function btn_scan(){
 		system_status.change_state(prev_state1);
 	}
 	
+}
+
+
+
+function btn_options(){
+	console.log("pressed pause/options button");
+	
+	//load current state
+	var state = system_status.state;
+	var prev_state1 = system_status.prev_state1;
+	var prev_state2 = system_status.prev_state2;
+	
+	if(state != "pause"){
+		system_status.change_state("pause");
+	}else{
+		system_status.change_state(prev_state1);
+	}
+}
+
+
+/*
+ * INVENTORY BUTTONS
+ */
+function btn_use(){
+	console.log("pressed use button");
 	
 }
+
+
+function btn_share(){
+	console.log("pressed share button");
+	
+}
+
+function btn_delete(){
+	console.log("pressed delete button");
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
