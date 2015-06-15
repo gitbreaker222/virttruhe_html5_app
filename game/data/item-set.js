438beaebe6410080612489dcbd1b646f66ad8b9d
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2015-06-15 11:59:48 +0200

    changed item_set.js to new item set

--- game/data/item-set.js
+++ game/data/item-set.js
@@ -4,125 +4,109 @@
  */
 var item_set = {
 		manual : {
-			"id": "manual",
-			"name": "Manual",
-			"image": "data/img/items/small/manual.png",
-			"icon": "data/img/items/small/manual.png"
-		},
-		arrows : {
-			"id": 		"arrows",
-			"name": 	"Arrows",
-			"image": 	"data/img/items/small/arrows.png",
-			"icon": 	"data/img/items/small/arrows.png",
-		},
-		deku_nut : {
-			"id": "deku_nut",
-			"name": "Deku Nut",
-			"image": "data/img/items/small/deku_nut.png",
-			"icon": "data/img/items/small/deku_nut.png"
-		},
-		deku_stick : {
-			"id": "deku_stick",
-			"name": "Deku Stick",
-			"image": "data/img/items/small/deku_stick.png",
-			"icon": "data/img/items/small/deku_stick.png"
-		},
-		deku_seeds : {
-			"id": "deku_seeds",
-			"name": "Deku Seeds",
-			"image": "data/img/items/small/deku_seeds.png",
-			"icon": "data/img/items/small/deku_seeds.png"
-		},
-		deku_shield : {
-			"id": "deku_shield",
-			"name": "Deku Shield",
-			"image": "data/img/items/small/deku_shield.png",
-			"icon": "data/img/items/small/deku_shield.png"
-		},
-		deku_sword : {
-			"id": "deku_sword",
-			"name": "Deku Sword",
-			"image": "data/img/items/small/deku_sword.png",
-			"icon": "data/img/items/small/deku_sword.png"
-		},
-		normal_boots : {
-			"id": "normal_boots",
-			"name": "Normal Boots",
-			"image": "data/img/items/small/boots_normal.png",
-			"icon": "data/img/items/small/boots_normal.png"
-		},
-		magic_beans : {
-			"id": "magic_beans",
-			"name": "Magic Beans",
-			"image": "data/img/items/small/magic_beans.png",
-			"icon": "data/img/items/small/magic_beans.png"
-		},
-		ocarina : {
-			"id": "ocarina",
-			"name": "Ocarina",
-			"image": "data/img/items/small/ocarina.png",
-			"icon": "data/img/items/small/ocarina.png"
-		},
-		empty_bootle : {
-			"id": "empty_bottle",
-			"name": "Empty Bottle",
-			"image": "data/img/items/small/bottle_empty.png",
-			"icon": "data/img/items/small/bottle_empty.png"
-		},
-		health : {
-			"id": "health",
-			"name": "Health Potion",
-			"image": "data/img/items/small/bottle_health.png",
-			"icon": "data/img/items/small/bottle_health.png"
-		},
-		boomerang : {
-			"id": "boomerang",
-			"name": "Boomerang",
-			"image": "data/img/items/small/boomerang.png",
-			"icon": "data/img/items/small/boomerang.png"
-		},
-		beer : {
-			"id": "beer",
-			"name": "Bier",
-			"image": "data/img/items/small/bottle_beer.png",
-			"icon": "data/img/items/small/bottle_beer.png"
-		},
-		lukas : {
-			"id": "lukas",
-			"name": "Nadelsturm",
-			"image": "data/img/items/small/lukas.png",
-			"icon": "data/img/items/small/lukas.png"
-		},
-		tanja : {
-			"id": "tanja",
-			"name": "Sving",
-			"image": "data/img/items/small/sving.jpg",
-			"icon": "data/img/items/small/sving.jpg"
-		},
-		rupee : {
-			"id": "rupee",
-			"name": "Rupee",
-			"image": "data/img/items/small/rupee.png",
-			"icon": "data/img/items/small/rupee.png"
-		},
-		test2 : {
-			"id": "test2",
-			"name": "Test Icon",
-			"image": "data/img/items/small/sold_out.png",
-			"icon": "data/img/items/small/sold_out.png"
-		},
-		test3 : {
-			"id": "test3",
-			"name": "Test Icon",
-			"image": "data/img/items/small/sold_out.png",
-			"icon": "data/img/items/small/sold_out.png"
-		},
-		test4 : {
-			"id": "test4",
-			"name": "Test Icon",
-			"image": "data/img/items/small/sold_out.png",
-			"icon": "data/img/items/small/sold_out.png"
-		},
+        "id"        : "manual", //TODO check if "id" is used and remove it (technically unnessecarry)
+        "name"      : "Manual",
+        "image"     : "data/img/items/manual.png",
+        "icon"      : "data/img/items/small/manual.png",
+        "stackable" : "false",
+        "use"       : "read"
+    },
+    short_summary : {
+        "id" : "short_summary",
+        "name" : "Short Summary",
+        "image" : "data/img/items/memory-card_01.png",
+        "icon" : "data/img/items/small/memory-card_01.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    sector : {
+        "id" : "sector",
+        "name" : "Sector",
+        "image" : "data/img/items/memory-card_02.png",
+        "icon" : "data/img/items/small/memory-card_02.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    detailed_description : {
+        "id" : "detailed_description",
+        "name" : "Detailed description",
+        "image" : "data/img/items/memory-card_03.png",
+        "icon" : "data/img/items/small/memory-card_03.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    aim : {
+        "id" : "aim",
+        "name" : "Aim",
+        "image" : "data/img/items/memory-card_04.png",
+        "icon" : "data/img/items/small/memory-card_04.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    target_group : {
+        "id" : "target_group",
+        "name" : "Target group",
+        "image" : "data/img/items/memory-card_05.png",
+        "icon" : "data/img/items/small/memory-card_05.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    what_is_new : {
+        //"id" : "what_is_new",
+        "name" : "What is new",
+        "image" : "data/img/items/memory-card_06.png",
+        "icon" : "data/img/items/small/memory-card_06.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    benefits_danube_region : {
+        //"id" : "benefits_danube_region",
+        "name" : "Benefits for Danube region",
+        "image" : "data/img/items/memory-card_07.png",
+        "icon" : "data/img/items/small/memory-card_07.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    technology : {
+        //"id" : "technology",
+        "name" : "Technology",
+        "image" : "data/img/items/memory-card_08.png",
+        "icon" : "data/img/items/small/memory-card_08.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    user_friendliness : {
+        //"id" : "user_friendliness",
+        "name" : "User friendliness",
+        "image" : "data/img/items/memory-card_09.png",
+        "icon" : "data/img/items/small/memory-card_09.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    next_steps : {
+        //"id" : "next_steps",
+        "name" : "Next steps",
+        "image" : "data/img/items/memory-card_10.png",
+        "icon" : "data/img/items/small/memory-card_10.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    business_plan : {
+        //"id" : "business_plan",
+        "name" : "Business plan",
+        "image" : "data/img/items/memory-card_11.png",
+        "icon" : "data/img/items/small/memory-card_11.png",
+        "stackable" : "false",
+        "use" : "read"
+    },
+    background_and_contact : {
+        //"id" : "background_and_contact",
+        "name" : "Background and contact info",
+        "image" : "data/img/items/memory-card_12.png",
+        "icon" : "data/img/items/small/memory-card_12.png",
+        "stackable" : "false",
+        "use" : "read"
+    }
 };
 
 
@@ -132,9 +116,7 @@ var item_set = {
  * they don't land in the inventory, but affect the players status
  */
 var attr_items = [
-	"heart",
-	"key",
-	"rupee",
+	"heart"
 ];
 	
 
@@ -147,26 +129,15 @@ var layers = {
 	//"id"				: "display-name"
 	"title"				:   {
 							"name" : "Reality",
-							"music" : undefined,
+							"music" : null
 							},
 	"reality"			:   {
 							"name" : "Reality",
-							"music" : undefined,
+							"music" : null
 							},
-	"kokiri_forest"		:   {
-							"name" : "Kokiri Forest",
-							"music" : "The Legend of Zelda - Kokiri Woods.mp3",
+	"secret"		:   {
+							"name" : "Secret Layer",
+							"music" : ""
 							},
-	"shop"				:   {
-							"name" : "Shop",
-							"music" : "Zelda - Laden.mp3",
-							},
-	"gerudo_valley"		:   {
-							"name" : "Gerudo Valley",
-							"music" : "The Legend of Zelda - Gerudo Valley.mp3",
-							},
-	"spirit_temple"		:   {
-							"name" : "Spirit Temple",
-							"music" : "The Legend of Zelda - Spirit Temple.mp3",
-							},
+	
 };
