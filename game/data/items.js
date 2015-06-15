3a90bf161b3068ed62b21f1c5adbd62ba4c70af9
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2015-06-15 11:49:35 +0200

    fit item img path to name

--- game/data/items.js
+++ game/data/items.js
@@ -10,96 +10,96 @@ var items = {
     short_summary : {
         "id" : "short_summary",
         "name" : "Short Summary",
-        "image" : "data/img/items/01.png",
-        "icon" : "data/img/items/small/01.png",
+        "image" : "data/img/items/memory-card_01.png",
+        "icon" : "data/img/items/small/memory-card_01.png",
         "stackable" : "false",
         "use" : "read"
     },
     sector : {
         "id" : "sector",
         "name" : "Sector",
-        "image" : "data/img/items/02.png",
-        "icon" : "data/img/items/small/02.png",
+        "image" : "data/img/items/memory-card_02.png",
+        "icon" : "data/img/items/small/memory-card_02.png",
         "stackable" : "false",
         "use" : "read"
     },
     detailed_description : {
         "id" : "detailed_description",
         "name" : "Detailed description",
-        "image" : "data/img/items/03.png",
-        "icon" : "data/img/items/small/03.png",
+        "image" : "data/img/items/memory-card_03.png",
+        "icon" : "data/img/items/small/memory-card_03.png",
         "stackable" : "false",
         "use" : "read"
     },
     aim : {
         "id" : "aim",
         "name" : "Aim",
-        "image" : "data/img/items/04.png",
-        "icon" : "data/img/items/small/04.png",
+        "image" : "data/img/items/memory-card_04.png",
+        "icon" : "data/img/items/small/memory-card_04.png",
         "stackable" : "false",
         "use" : "read"
     },
     target_group : {
         "id" : "target_group",
         "name" : "Target group",
-        "image" : "data/img/items/05.png",
-        "icon" : "data/img/items/small/05.png",
+        "image" : "data/img/items/memory-card_05.png",
+        "icon" : "data/img/items/small/memory-card_05.png",
         "stackable" : "false",
         "use" : "read"
     },
     what_is_new : {
         //"id" : "what_is_new",
         "name" : "What is new",
-        "image" : "data/img/items/06.png",
-        "icon" : "data/img/items/small/06.png",
+        "image" : "data/img/items/memory-card_06.png",
+        "icon" : "data/img/items/small/memory-card_06.png",
         "stackable" : "false",
         "use" : "read"
     },
     benefits_danube_region : {
         //"id" : "benefits_danube_region",
         "name" : "Benefits for Danube region",
-        "image" : "data/img/items/07.png",
-        "icon" : "data/img/items/small/07.png",
+        "image" : "data/img/items/memory-card_07.png",
+        "icon" : "data/img/items/small/memory-card_07.png",
         "stackable" : "false",
         "use" : "read"
     },
     technology : {
         //"id" : "technology",
         "name" : "Technology",
-        "image" : "data/img/items/08.png",
-        "icon" : "data/img/items/small/08.png",
+        "image" : "data/img/items/memory-card_08.png",
+        "icon" : "data/img/items/small/memory-card_08.png",
         "stackable" : "false",
         "use" : "read"
     },
     user_friendliness : {
         //"id" : "user_friendliness",
         "name" : "User friendliness",
-        "image" : "data/img/items/09.png",
-        "icon" : "data/img/items/small/09.png",
+        "image" : "data/img/items/memory-card_09.png",
+        "icon" : "data/img/items/small/memory-card_09.png",
         "stackable" : "false",
         "use" : "read"
     },
     next_steps : {
         //"id" : "next_steps",
         "name" : "Next steps",
-        "image" : "data/img/items/10.png",
-        "icon" : "data/img/items/small/10.png",
+        "image" : "data/img/items/memory-card_10.png",
+        "icon" : "data/img/items/small/memory-card_10.png",
         "stackable" : "false",
         "use" : "read"
     },
     business_plan : {
         //"id" : "business_plan",
         "name" : "Business plan",
-        "image" : "data/img/items/11.png",
-        "icon" : "data/img/items/small/11.png",
+        "image" : "data/img/items/memory-card_11.png",
+        "icon" : "data/img/items/small/memory-card_11.png",
         "stackable" : "false",
         "use" : "read"
     },
     background_and_contact : {
         //"id" : "background_and_contact",
         "name" : "Background and contact info",
-        "image" : "data/img/items/12.png",
-        "icon" : "data/img/items/small/12.png",
+        "image" : "data/img/items/memory-card_12.png",
+        "icon" : "data/img/items/small/memory-card_12.png",
         "stackable" : "false",
         "use" : "read"
     }
