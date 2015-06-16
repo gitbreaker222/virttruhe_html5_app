a90a4082f482e1a501d3b6138a8776c42a8bdb0a
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2015-06-16 03:44:37 +0200

    adjust dialog on close behavior

--- game/view/dialog.js
+++ game/view/dialog.js
@@ -1,14 +1,22 @@
 dialog = {
-    init	: function(){
-		//hide all dialogs
-		$(".dialog").hide();
-		//apply background click for close to each
-		//$(".dialog").attr("onclick", "$(this).hide()");
-		//fix background click for new_item dialog
-		$("#d_ni").attr("onclick", "");
-	},
-	
-	close  : function(){
-	    $(".d_active").hide().removeClass("d_active");
-	}
+    init : function() {
+        //hide all dialogs
+        $(".dialog").hide();
+        //apply background click for close to each
+        //$(".dialog").attr("onclick", "$(this).hide()");
+        //fix background click for new_item dialog
+        $("#d_ni").attr("onclick", "");
+    },
+
+    close : function() {
+        $(".d_active").hide();
+        
+        //if dialog has an audio track, then stop/reset it
+        if ($(".d_active audio")[0]) {
+            $(".d_active audio")[0].pause();
+            $(".d_active audio")[0].currentTime = 0;
+            $(".d_active audio").removeClass("d_active");
+        }
+
+    }
 };
