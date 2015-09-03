4416f4e687aae67dcdca7e20bc2da9c73bc3c7fb
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2015-09-03 17:06:34 +0200

    created keymap ods -> csv -> json

--- game/Control/init.js
+++ game/Control/init.js
@@ -3,15 +3,24 @@
  */
 function init(){
     console.log("=====INIT=====");
-    $(document).foundation();
+    $(document).foundation(); //init for foundation framework or so
+
+    //get JSON files
     $.getJSON('Data/items.json', function(data){
         //save JSON to new global object
         items = {};
         for(i in data){
             items[data[i].id] = data[i];
         }
+    });
+    $.getJSON('Data/key_map.json', function(data){
+        //save JSON to new global object
+        key_map = {};
+        for(i in data){
+            key_map[data[i].key] = data[i];
+        }
 
-    })
+    });
 }
 
 $(document).ready(init());
